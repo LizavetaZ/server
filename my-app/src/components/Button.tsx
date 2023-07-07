c86e7c5d37a930980da57452f80322a442b9236f
LizavetaZ <vokibzoj@yandex.by>
2023-07-07 19:49:57 +0300

    server work

---
+++ my-app/src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export type PropsType = {
+    name: string
+    callBack: () => void
+}
+
+
+export const Button = (props: PropsType) => {
+
+    const onClickHandler = () => {
+        props.callBack()
+    }
+
+    return (
+        <button onClick={onClickHandler}>{props.name}</button>
+    );
+};
